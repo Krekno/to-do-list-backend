e0d2a31f728024eef4d2c85d7e3f8bfc716180f7
Krekno <yigiterenkarabas@gmail.com>
2025-07-26 14:09:24 +0300

    add models

---
+++ src/main/java/com/krekno/todolistbackend/model/Task.java
@@ -0,0 +1,21 @@
+package com.krekno.todolistbackend.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "tasks")
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    @ManyToOne
+    private User user;
+}
---
+++ src/main/java/com/krekno/todolistbackend/model/User.java
@@ -0,0 +1,25 @@
+package com.krekno.todolistbackend.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    private String username;
+
+    private String password;
+
+    @OneToMany(mappedBy = "user")
+    private List<Task> tasks;
+}
